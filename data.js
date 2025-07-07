6a1a8aa7640840def9e8b9b199310199bc30210d
samzmorrison <samzmorrison@gmail.com>
2025-07-07 03:13:39 -0700

    Update data.js

--- data.js
+++ data.js
@@ -30,7 +30,7 @@ var APP_DATA = {
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.0
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
